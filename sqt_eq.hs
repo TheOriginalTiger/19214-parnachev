a1b2e32065bf94064f1442515829baf7257eca11
Tgr <vvp.vovarulit@gmail.com>
2019-10-01 20:29:04 +0700

    allright got it. pattern matching > guards

--- sqt_eq.hs
+++ sqt_eq.hs
@@ -1,8 +1,9 @@
-func a b c  | a == 0 && b/= 0 = ( (-1) * c / b, (-1) * c / b)
-            | a == 0 && b == 0 = error "no roots"
-            | d < 0 = error "no real roots"
+func 0 b c = ( (-1) * c / b, (-1) * c / b)
+
+
+func a b c  | d < 0 = error "no real roots"
             | otherwise = (x1, x2) 
             where 
                 d = b * b - 4 * a * c
-                x1 = ((-1) * b + sqrt d)/(2*a)
-                x2 = ((-1) * b - sqrt d)/(2*a)
+                x1 = (-b + sqrt d)/(2*a)
+                x2 = (-b - sqrt d)/(2*a)
