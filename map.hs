bb80aedd015d43ca83b06e1ee8e27e502821581c
Tgr <vvp.vovarulit@gmail.com>
2019-10-08 22:30:38 +0700

    added an answer for the question about prefferences

--- map.hs
+++ map.hs
@@ -4,7 +4,11 @@ double :: Integer -> Integer
 reversing :: [Integer]->[Integer]
 
 double x = 2 * x 
-reversing arr = foldl (\x y -> y:x) [] arr
+reversing arr = foldl (\x y -> y:x) [] arr 
 mapl f xs = reversing (foldl (\n y -> f y : n) [] xs)
 
 mapr f xs = foldr (\x y -> f x : y) [] xs
+{- из-за необходимисоти повторной итерации по массиву в случае 
+foldl (при её отсуствии массив будет задом наперед) очевидно, что вариант foldr  
+намного лучше
+-}
