190053bb27afed170711d92b4821abbe8e3f08c2
Tgr <vvp.vovarulit@gmail.com>
2019-10-17 15:25:46 +0700

    added some error handlers to lists.hs

--- lists.hs
+++ lists.hs
@@ -1,5 +1,5 @@
 --1
-myget :: [a] -> Int -> a
+myget :: [a] -> Integer -> a
 myget [] n = error "no such element in list"
 myget (x:xs) 0 = x 
 myget (x:xs) n = myget xs (n-1)
@@ -11,6 +11,7 @@ myhead (x:xs) = x
 
 --3
 mylast :: [a] -> a
+--myreverse
 mylast xs = myhead (foldl (\x y ->  y:x) [] xs)
 
 --4
@@ -30,7 +31,7 @@ myreverse [] = error "empty list"
 myreverse xs = foldl (\x y -> y:x) [] xs
 
 --7 
-mylength :: [a] -> Int
+mylength :: [a] -> Integer
 mylength xs = foldr (\x y -> y + 1) 0 xs 
 
 --8 
@@ -44,22 +45,33 @@ myconcat [x] sys = x:sys
 myconcat (x:xs) sys = x: myconcat xs sys
 
 --10
-mydrop :: Int -> [a] -> [a]
+-- match errors
+mydrop :: Integer -> [a] -> [a]
 mydrop 0 xs = xs
+mydrop n [] = error "list index out of range"
 mydrop n (x:xs) = mydrop (n-1) xs
 
 --11
-mytake :: Int->[a]->[a]
+--errors
+mytake :: Integer->[a]->[a]
 mytake 0 xs = []
+mytake n [] = error "list index out of range"
 mytake n (x:xs) = x: mytake (n-1) xs
 
---12
-mysplit :: Int->[a]->[[a]]
-mysplit n xs = [left n xs, right n xs] where -- also could use mytake & mydrop but its too boring 
+--12    
+-- () instead 
+-- make more faster
+mysplit :: Integer->[a]->([a], [a])
+mysplit n xs = (left n xs, right n xs) where -- also could use mytake & mydrop but its too boring 
                 left 0 xs = [] -- monkeycoding!
                 left n (l:ls) = l: left (n-1) ls --everywhere!
                 right 0 rs = rs --hooray!
                 right k (r:rs) = right (k-1) rs --I love it
+splitAt' :: Integer -> [a] -> ([a], [a])
+
+
+splitAt' n xs = ()
+
 
 --13
 mynull :: [a]->Bool
@@ -75,7 +87,7 @@ myelem (x:xs) y | x == y = True
 --15
 myfilter :: (a->Bool)->[a]->[a]
 myfilter tst [] = []
-myfilter tst (x:xs) = if (tst x) then x: (myfilter tst xs) else myfilter tst xs --duno how to do ir more declarative
+myfilter tst (x:xs) = if tst x then x: (myfilter tst xs) else myfilter tst xs --duno how to do ir more declarative
 
 --16
 mymap :: (a -> b)->[a] -> [b]
