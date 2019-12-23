e41e8741c95b2d161c6716d7aed87716ed7a180f
Tgr <vvp.vovarulit@gmail.com>
2019-12-23 21:12:54 +0700

    only io is left to implement

--- labs/hashTable.hs
+++ labs/hashTable.hs
@@ -1,6 +1,8 @@
 import Data.Char
 import Data.List
 import Data.Tuple
+import System.IO
+import System.Exit
 
 type Capacity = Int
 type Loadfactor = Double
@@ -19,7 +21,7 @@ hash x = foldr (\x xs -> xs + ord x) 0 (show x)
 updateLF :: HashTable k v-> HashTable k v 
 updateLF (ToHashTable table c l e) = ToHashTable table c ((fromIntegral e) / (fromIntegral c)) e
  
-checkLF :: (Eq k, Show k)=> HashTable k v-> HashTable k v
+checkLF :: (Eq k, Show k) => HashTable k v-> HashTable k v
 checkLF (ToHashTable table c l e) | l > 0.5 = fromList $ concat table
                                   | otherwise = ToHashTable table c l e
 
@@ -35,36 +37,45 @@ clear :: HashTable k v -> HashTable k v
 clear (ToHashTable table c l e)  =  ToHashTable (replicate c []) c l e 
 
 insertToTable::(Show k, Eq k)=>HashTable k v -> k -> v-> HashTable k v
-insertToTable (ToHashTable table c l e) key value  = checkLF $ updateLF $ ToHashTable
-                                 ([ if mod (hash key) c  == fst li
-                                         then (snd li) ++ [(key,value)]
-                                    else snd li | li <- enumerate table
-                                   ])
-                                 c l (e + 1) 
+insertToTable (ToHashTable table c l e) key value  = checkLF $ updateLF $ ToHashTable (map
+                                ( \(i,o) -> if i == mod (hash key) c then ([el| el<-o, fst el /= key] ++ [(key,value)]) else o ) 
+                                (enumerate table)) 
+                                c l (e + 1) 
 
 fromList::(Show k, Eq k) => [(k,v)]->HashTable k v
 fromList list = foldr (\h hs-> insertToTable hs (fst h) (snd h)) (customHashTable $ length list * 2 + 1) list
 
 -- Удаляет элементы по заданному ключу.
 erase ::(Show k, Eq k)=>HashTable k v->k->HashTable k v
-erase (ToHashTable table c l e) key = ToHashTable (map
+erase (ToHashTable table c l e) key = updateLF $ ToHashTable (map
                                ( \(i,o) -> if i == mod (hash key) c then [el| el<-o, fst el /= key] else o ) (enumerate table) )
-                                 c l (length $ concat table)
+                                 c l (e - 1)
 
 
 getSamelyHashed :: (Show k, Eq k)=>HashTable k v ->k-> [(k,v)]
 getSamelyHashed (ToHashTable table c l e) key = concat $ [ o | (i,o) <- enumerate table, i == mod (hash key) c]
+
 -- Проверка наличия значения по заданному ключу.
 contains::(Show k, Eq k)=>HashTable k v -> k -> Bool;
 contains table key = elem key (map fst (getSamelyHashed table key) )
 
 -- Возвращает значение по ключу.
 at::(Show k, Eq k)=>HashTable k v -> k -> Maybe v
-at table key | contains table key = Just $ head [v | (k,v) <- (getSamelyHashed table key) ] 
+at table key | contains table key = Just $ head [v | (k,v) <- (getSamelyHashed table key), k == key ] 
              | otherwise = Nothing
 
-size::(Show k, Eq k)=>HashTable k v -> Integer;
+size::(Show k, Eq k)=>HashTable k v -> Int;
 size (ToHashTable _ _ _ e) = e
 
 empty::(Show k, Eq k)=>HashTable k v -> Bool;
 empty(ToHashTable _ _ _ e ) = e > 0
+
+{-
+main :: IO()
+main = withFile "input.txt" ReadMode (\h-> do
+                                            filelines <- hGetContents h >>= lines
+                                            let x = defaultHashTable
+                                            --let data = [(head xs, last xs), xs<-content]
+                                            print content                                           
+  )
+-}
